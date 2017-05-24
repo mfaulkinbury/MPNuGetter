67fcef14432990f926b449d6fcae45c738f873f4
mfaulkinbury <mfaulkinbury@3F094838H>
2017-05-24 15:07:56 -0500

    Refactored to remove use of props file. Script uses VSAE through refection to add/remove references

---
+++ src/templates/CustomInstall.ps1
@@ -0,0 +1,3 @@
+param($installPath, $toolsPath, $package, $project)
+
+# add your custom install code here
---
+++ src/templates/CustomUninstall.ps1
@@ -0,0 +1,3 @@
+param($installPath, $toolsPath, $package, $project)
+
+# Add your custom uninstall code here
